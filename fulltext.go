7f72e87293cd327a10fe8f46d2e59258e46a3c9f
Ian Lopshire <ianlopshire@me.com>
2018-04-29 18:13:25 -0400

    Add Document and Library interfaces

--- fulltext.go
+++ fulltext.go
@@ -1,3 +1,46 @@
 // Package fulltext aims to provide a set of interfaces and common implementations for performing
 // full-text search across a set of documents.
 package fulltext
+
+import (
+	"errors"
+	"io"
+)
+
+// DocumentToken identifies a document.
+//
+// A DocumentToken should be unique within a library.
+type DocumentToken string
+
+// Document represents a single document.
+type Document interface {
+	// Token returns the documents token.
+	Token() DocumentToken
+
+	// Reader returns an io.Reader that accesses the document data or an error.
+	Reader() (io.Reader, error)
+}
+
+var (
+	ErrDocNotFound = errors.New("document not found")
+)
+
+// Library defines a set of documents.
+type Library interface {
+	// Find returns the document with the given token or an error.
+	//
+	// If a document is not found, Find should return ErrDocNotFound.
+	Find(DocumentToken) (Document, error)
+
+	// List returns a list of documents or an error.
+	//
+	// The count param controlls the maximum number of documents returned.
+	// The offset param controlls the index where the returned list starts.
+	//
+	// Calling List should return consistent results given a specific count
+	// and offset.
+	List(count, offset int) ([]Document, error)
+
+	// Count should return the number of documents in a library or an error.
+	Count() (int, error)
+}
