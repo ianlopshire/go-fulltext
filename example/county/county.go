d81e74a12aa10fb1a83619e779c4571ccfe1a316
Ian Lopshire <ianlopshire@me.com>
2018-04-29 16:18:00 -0400

    Add county example directory and data

---
+++ example/county/county.go
@@ -0,0 +1,6 @@
+// Package county contains example implementations around the county search problem.
+//
+// The goal of county search is to surface relevant counties from a list of counties in the Unites States.
+// This problem provides a library of documents that is quite small. The average size of each document is
+// also quite small.
+package county
